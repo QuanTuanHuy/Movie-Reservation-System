fa315994c93668cf26a262a8ea13ce7fdfccbad4
Quan Huy <quan.tuan.huy@gmail.com>
2024-09-14 22:53:38 +0700

    fix statistic

--- src/main/java/hust/project/moviereservationsystem/usecase/GetStatisticUseCase.java
+++ src/main/java/hust/project/moviereservationsystem/usecase/GetStatisticUseCase.java
@@ -1,12 +1,12 @@
 package hust.project.moviereservationsystem.usecase;
 
-import hust.project.moviereservationsystem.entity.BaseEntity;
-import hust.project.moviereservationsystem.entity.BookingEntity;
-import hust.project.moviereservationsystem.entity.ShowEntity;
+import hust.project.moviereservationsystem.constant.BookingStatus;
+import hust.project.moviereservationsystem.entity.*;
 import hust.project.moviereservationsystem.entity.request.GetStatisticRequest;
 import hust.project.moviereservationsystem.entity.response.StatisticResponse;
 import hust.project.moviereservationsystem.port.IBookingPort;
 import hust.project.moviereservationsystem.port.ICinemaHallPort;
+import hust.project.moviereservationsystem.port.IPaymentPort;
 import hust.project.moviereservationsystem.port.IShowPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,27 +20,31 @@ public class GetStatisticUseCase {
     private final IShowPort showPort;
     private final ICinemaHallPort cinemaHallPort;
     private final IBookingPort bookingPort;
+    private final IPaymentPort paymentPort;
 
     public StatisticResponse getStatistic(GetStatisticRequest request) {
         List<Long> cinemaHallIds = new ArrayList<>();
         if (request.getCinemaId() != null) {
             cinemaHallIds = cinemaHallPort.getCinemaHallsByCinemaId(request.getCinemaId())
                     .stream()
-                    .map(BaseEntity::getId)
+                    .map(CinemaHallEntity::getId)
                     .toList();
         }
 
         List<ShowEntity> shows = showPort.getShowsForStatistic(request, cinemaHallIds);
         List<Long> showIds = shows.stream().map(ShowEntity::getId).toList();
 
-        List<BookingEntity> bookings = bookingPort.getBookingsByShowIds(showIds);
+        List<BookingEntity> bookings = bookingPort.getBookingsByShowIds(showIds)
+                .stream()
+                .filter(booking -> booking.getStatus().equals(BookingStatus.COMPLETED.name()))
+                .toList();
 
-        Long revenue = 0L, totalTickets = 0L;
+        List<PaymentEntity> payments = paymentPort.getPaymentsByBookingIds(
+                bookings.stream().map(BookingEntity::getId).toList()
+        );
 
-        for (var booking : bookings) {
-            revenue += booking.getTotalPrice();
-            totalTickets += booking.getTotalSeats();
-        }
+        Long revenue = payments.stream().map(PaymentEntity::getFinalAmount).reduce(0L, Long::sum);
+        Long totalTickets = bookings.stream().map(BookingEntity::getTotalSeats).reduce(0L, Long::sum);
 
         return new StatisticResponse(revenue, totalTickets);
     }
